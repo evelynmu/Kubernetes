1751ff5afddd95bb76cbc20c0ce4cb270c0efe1b
Lorena Muñoz Nava <128324706+evelynmu@users.noreply.github.com>
2025-04-03 17:36:34 -0600

    Create print_n_numbers.py

---
+++ python/print_n_numbers.py
@@ -0,0 +1,9 @@
+try:
+    n = 10
+    print("El numero seleccionado fue:",n)
+    
+    for x in range(n):
+        print(x)
+      
+except ValueError:
+  print("Error: No se puede converti el parametro proporcioando a entero.")
